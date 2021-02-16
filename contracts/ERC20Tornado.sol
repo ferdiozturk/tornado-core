fd2512d01c7ac9382b86e6ddcfdaf86e0b379388
Ferdi Öztürk <foerdi@gmail.com>
2021-02-16 18:38:56 +0100

    Update ERC20Tornado.sol

--- contracts/ERC20Tornado.sol
+++ contracts/ERC20Tornado.sol
@@ -1,19 +1,8 @@
-// https://tornado.cash
-/*
-* d888888P                                           dP              a88888b.                   dP
-*    88                                              88             d8'   `88                   88
-*    88    .d8888b. 88d888b. 88d888b. .d8888b. .d888b88 .d8888b.    88        .d8888b. .d8888b. 88d888b.
-*    88    88'  `88 88'  `88 88'  `88 88'  `88 88'  `88 88'  `88    88        88'  `88 Y8ooooo. 88'  `88
-*    88    88.  .88 88       88    88 88.  .88 88.  .88 88.  .88 dP Y8.   .88 88.  .88       88 88    88
-*    dP    `88888P' dP       dP    dP `88888P8 `88888P8 `88888P' 88  Y88888P' `88888P8 `88888P' dP    dP
-* ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
-*/
-
 pragma solidity 0.5.17;
 
-import "./Tornado.sol";
+import "./Twister.sol";
 
-contract ERC20Tornado is Tornado {
+contract BEP20Twister is Twister {
   address public token;
 
   constructor(
@@ -27,16 +16,16 @@ contract ERC20Tornado is Tornado {
   }
 
   function _processDeposit() internal {
-    require(msg.value == 0, "ETH value is supposed to be 0 for ERC20 instance");
-    _safeErc20TransferFrom(msg.sender, address(this), denomination);
+    require(msg.value == 0, "BNB value is supposed to be 0 for BEP20 instance");
+    _safeBep20TransferFrom(msg.sender, address(this), denomination);
   }
 
   function _processWithdraw(address payable _recipient, address payable _relayer, uint256 _fee, uint256 _refund) internal {
     require(msg.value == _refund, "Incorrect refund amount received by the contract");
 
-    _safeErc20Transfer(_recipient, denomination - _fee);
+    _safeBep20Transfer(_recipient, denomination - _fee);
     if (_fee > 0) {
-      _safeErc20Transfer(_relayer, _fee);
+      _safeBep20Transfer(_relayer, _fee);
     }
 
     if (_refund > 0) {
@@ -48,7 +37,7 @@ contract ERC20Tornado is Tornado {
     }
   }
 
-  function _safeErc20TransferFrom(address _from, address _to, uint256 _amount) internal {
+  function _safeBep20TransferFrom(address _from, address _to, uint256 _amount) internal {
     (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd /* transferFrom */, _from, _to, _amount));
     require(success, "not enough allowed tokens");
 
@@ -60,7 +49,7 @@ contract ERC20Tornado is Tornado {
     }
   }
 
-  function _safeErc20Transfer(address _to, uint256 _amount) internal {
+  function _safeBep20Transfer(address _to, uint256 _amount) internal {
     (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb /* transfer */, _to, _amount));
     require(success, "not enough tokens");
 
